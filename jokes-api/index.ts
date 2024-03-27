aa25438d6642da0835ec7f297deb4ed48a42f722
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-03-27 12:29:41 +0000

    ✨ Write even more nonsense typescript code

--- jokes-api/index.ts
+++ jokes-api/index.ts
@@ -49,3 +49,51 @@ console.log(capitalize('hello'));
 const multiply = (a: number, b: number): number => a * b;
 
 console.log(multiply(5, 6));
+
+const reverseString = (str: string): string => {
+    return str.split('').reverse().join('');
+};
+console.log(reverseString('hello'));
+
+const calculateFactorial = (num: number): number => {
+    if (num === 0 || num === 1) {
+        return 1;
+    } else {
+        return num * calculateFactorial(num - 1);
+    }
+};
+console.log(calculateFactorial(5));
+
+const isPalindrome = (str: string): boolean => {
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+};
+console.log(isPalindrome('madam'));
+
+const findMaxNumber = (numbers: number[]): number => {
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+};
+console.log(findMaxNumber([10, 5, 20, 15]));
+
+const calculateAverage = (numbers: number[]): number => {
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum / numbers.length;
+};
+console.log(calculateAverage([1, 2, 3, 4, 5]));
+
+const removeDuplicates = (arr: any[]): any[] => {
+    return [...new Set(arr)];
+};
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+const formatDate = (date: Date): string => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+};
+console.log(formatDate(new Date()));
